ad1f2c67814fe7c29f0cac55f326b3f346689563
lukasozpaker <44107295+lukasozpaker@users.noreply.github.com>
2021-11-08 13:31:02 -0500

    abstract (nav and sensor code factored)

---
+++ sensors-usable.ino
@@ -0,0 +1,30 @@
+#define echoPin 2 
+#define trigPin 3 
+
+
+void setup() {
+    pinMode(trigPin, OUTPUT); 
+    pinMode(echoPin, INPUT); 
+}
+void loop() {
+ 
+}
+
+
+double sideX(double yDist, double sensorDist) { //in case of angled sensor
+    return sqrt(sensorDist * sensorDist - yDist * yDist);
+
+}
+double readUltrasonicSensor() {
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    
+    return pulseIn(echoPin, HIGH) * 0.34 / 2; //distance
+}
+
+
+
