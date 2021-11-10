002a98e2ecf239321eb9f798b2bb49c1d6c8c32e
lukasozpaker <44107295+lukasozpaker@users.noreply.github.com>
2021-11-10 12:36:43 -0500

    motor driver refactor

--- Motor_Driver_Code.ino
+++ Motor_Driver_Code.ino
@@ -16,70 +16,84 @@ void setup() {
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
   
-  // Turn off motors - Initial state
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, LOW);
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, LOW);
-}
+  brake();
 
-void loop() {
   directionControl();
   delay(1000);
   speedControl();
   delay(1000);
 }
 
+void loop() {
+  
+}
+
 // Spinning direction of motors
 void directionControl() {
-  // motors at max speed (255)
-  // 0 to 255
-  analogWrite(enA, 255);
-  analogWrite(enB, 255);
+  setBothAnalog();
 
-  // Turn on motor A & B
-  digitalWrite(in1, HIGH);
-  digitalWrite(in2, LOW);
-  digitalWrite(in3, HIGH);
-  digitalWrite(in4, LOW);
+  setReverseMotors();
   delay(2000);
   
-  // Change motor directions
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, HIGH);
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, HIGH);
+  setBothMotors();
   delay(2000);
   
-  // Turn off motors
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, LOW);
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, LOW);
+  brake();
 }
 
 // Speed of the motors
 void speedControl() {
+  
+  setBothMotors();
+  
+  accel();
+  decel();
+  
+  
+  brake();
+}
+
+void setBothAnalog() {
+  // motors at max speed (255)
+  // 0 to 255
+  analogWrite(enA, 255);
+  analogWrite(enB, 255);
+}
+void setBothMotors() {
   // Turn on motors
   digitalWrite(in1, LOW);
   digitalWrite(in2, HIGH);
   digitalWrite(in3, LOW);
   digitalWrite(in4, HIGH);
-  
+}
+
+void setReverseMotors() {
+  // Turn on motor A & B
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+}
+
+void accel() {
   // Accelerate from 0 to 255
   for (int i = 0; i < 256; i++) {
     analogWrite(enA, i);
     analogWrite(enB, i);
     delay(20);
   }
-  
+}
+
+void decel() {
   // Decelerate from 255 to 0
   for (int i = 255; i >= 0; --i) {
     analogWrite(enA, i);
     analogWrite(enB, i);
     delay(20);
   }
-  
+}
+
+void brake() {
   // Turn off motors
   digitalWrite(in1, LOW);
   digitalWrite(in2, LOW);
