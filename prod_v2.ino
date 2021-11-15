09dbd7dc7b6e6a527f18059c2161647c31480958
lukasozpaker <44107295+lukasozpaker@users.noreply.github.com>
2021-11-15 12:16:37 -0500

    v2

---
+++ prod_v2.ino
@@ -0,0 +1,126 @@
+//begin ultrasonic imports
+#define echoPin 2 
+#define trigPin 3 
+//end ultrasonic imports
+
+int in1 = 8;  //direction 1
+int in2 = 7;  //direction 1
+int ena = 9;  //power 1
+
+int in3 = 5;  //direction 2
+int in4 = 4;  //direction 2
+int enb = 3;  //power 2
+
+void setup() {
+  pinMode(in1, OUTPUT);  // sets the pin as output
+  pinMode(in2, OUTPUT); // sets the pin as output
+  pinMode(ena, OUTPUT);
+  
+  pinMode(in3, OUTPUT);  // sets the pin as output
+  pinMode(in4, OUTPUT); // sets the pin as output
+  pinMode(enb, OUTPUT);
+
+
+//   while(true) {
+//     setBothMotors(235);
+//     delay(1000);
+//     pivotCW(230);
+//     delay(1000);
+//     brake();
+//     delay(1000);
+//   }
+
+    for(int i=0;i<10;i++) {
+        forward();
+
+        brake();
+        sleep(500);
+
+        dumbAvoid();
+        sleep(500);
+    }
+    
+
+
+
+  
+}
+
+
+
+void loop() {
+   
+}
+
+void forward(){
+    while(!obstacle()) {
+     setBothMotors(235);
+    } 
+}
+void setBothMotors(int SPEED) {
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2,LOW);
+  analogWrite(ena,SPEED);
+
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4,LOW);
+  analogWrite(enb,SPEED);
+}
+
+void pivotCW(int SPEED) {
+  digitalWrite(in1, LOW);
+  digitalWrite(in2,HIGH);
+  analogWrite(ena,SPEED);
+
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4,LOW);
+  analogWrite(enb,SPEED);
+}
+
+void brake() {
+  digitalWrite(in1, LOW);
+  digitalWrite(in2,LOW);
+  analogWrite(ena,0);
+
+  digitalWrite(in3, LOW);
+  digitalWrite(in4,LOW);
+  analogWrite(enb,0);
+}
+
+bool obstacle() {
+  if(readUltrasonicSensor() < 20.0) {
+    brake();
+    return true;
+  }
+  return false;
+}
+
+double readUltrasonicSensor() {
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    
+    return pulseIn(echoPin, HIGH) * 0.34 / 2; //distance
+}
+
+void dumbAvoid() {
+    dumb90();
+    forward();
+    delay(300);
+    dumb90();
+    dumb90();
+    dumb90();
+    brake();
+}
+
+void dumb90() {
+    brake();
+    for(int i=0;i<3;i++) {
+        pivotCW();
+        sleep(200);
+    }
+    brake();
+}
