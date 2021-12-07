9bd7ee490a02c167df1080b3516639315f8fdba8
Michael Shipman <michael@shipman.ws>
2021-12-07 12:50:44 -0500

    Mission Voltage Sensing code
    Reads data from pin A0 and decodes ascii letter which can be accessed through the "deliverable" variable. This code also transmits to mission control

--- voltageSensor.ino
+++ voltageSensor.ino
@@ -1,3 +1,5 @@
+#include <Enes100.h>
+
 int i = 0;
 short initialStream = 0b1111111111111111;
 unsigned char dataStream = 0b11111111;
@@ -6,6 +8,7 @@ byte inBit;
 
 void setup() {
   Serial.begin(9600);
+  Enes100.begin("grrrrrrrrr", DATA, 3, 8, 9);
 }
 
 void loop() {
@@ -13,34 +16,38 @@ void loop() {
     delay(125);
 
     int sensorValue = analogRead(A0);
-    float voltage = sensorValue * (5.0 / 1023.0);
+    //Serial.println(sensorValue);
+    float voltage = sensorValue * (5.17 / 1024.0);
+    Serial.println(voltage);
   
     if ((initialStream & 0b111111111) != (code & 0b111111111)) {
-      if (voltage < 4.0 && voltage > 1.0) {
+      if (voltage < 4.0 && voltage > 0.75) {
         Serial.println("not connected");
       } else if (voltage >= 4.0 && voltage <= 5.0) {
         inBit = 1;
         initialStream = (initialStream << 1) + inBit;
-        Serial.println(1);
-      } else if (voltage >= 0.0 && voltage <= 1.0) {
+        //Serial.println(voltage);
+        //Serial.println(1);
+      } else if (voltage >= 0.0 && voltage <= 0.75) {
         inBit = 0;
         initialStream = (initialStream << 1) + inBit;
-        Serial.println(0);
+        //Serial.println(voltage);
+        //Serial.println(0);
       }
     } else {
       if (i <= 0) {
-        Serial.println("Activation Match");
+        //Serial.println("Activation Match");
       }
-      if (voltage < 4.0 && voltage > 1.0 && i < 8) {
-        Serial.println("not connected");
+      if (voltage < 4.0 && voltage > 0.75 && i < 8) {
+        //Serial.println("not connected");
       } else if (voltage >= 4.0 && voltage <= 5.0 && i < 8) {
         inBit = 1;
         dataStream = (dataStream << 1) + inBit;
-        Serial.println(1);
-      } else if (voltage >= 0.0 && voltage <= 1.0 && i < 8) {
+        //Serial.println(1);
+      } else if (voltage >= 0.0 && voltage <= 0.75 && i < 8) {
         inBit = 0;
         dataStream = (dataStream << 1) + inBit;
-        Serial.println(0);
+        //Serial.println(0);
       }
       i++;
     }
@@ -51,5 +58,7 @@ void loop() {
       Serial.println("Execution complete");
       char deliverable = dataStream;
       Serial.println(deliverable);
+      Enes100.println(deliverable);
+      Enes100.mission(deliverable);
     }
 }
